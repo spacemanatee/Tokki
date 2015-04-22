f8d12f25b05cb01990fa9a756cbe07780c5b0bcc
travisjn <tdizzletrav@gmail.com>
2015-04-22 11:47:19 -0700

    Add test functionality to question result controller

--- public/client/controllers/questionResultsController.js
+++ public/client/controllers/questionResultsController.js
@@ -2,7 +2,9 @@ angular.module('tokki')
   .controller('QuestionResultsController', ['$scope', '$state', '$location', function($scope, $state, $location) {
 
   $scope.init = function() {
-
+    console.log('Question controller Init now!');
+    $scope.testResult = 25678;
   };
 
+  $scope.init();
 }]);
